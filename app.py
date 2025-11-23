32ba13d62553b2169a953eb69251fc398e4e7db7
swapnil2348 <37963588+swapnil2348@users.noreply.github.com>
2025-11-23 09:13:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -98,6 +98,10 @@ def privacy():
 @app.route("/terms")
 def terms():
     return render_template("terms.html")
+    
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
